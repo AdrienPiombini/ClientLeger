5991ae0b2b0337de26ee4c3489dfb4ce27122b9d
AdrienPiombini <adrien.piombini@gmail.com>
2023-01-22 12:57:45 +0100

    prdt

--- Vue/espace_membre/vue_produits.php
+++ Vue/espace_membre/vue_produits.php
@@ -9,16 +9,18 @@
     <tr>
         <td></td>
         <td>ID Produits</td>
+        <td>Nom produit</td> 
         <td>Quantite en stock</td> 
     </tr>
 
     <?php
         foreach($les_produits as $un_produit){
             echo"<form method='post'><tr>";
-            echo"<td><img src='img/user.png'></td>";
-            echo"<td>".$un_produit['idproduit']."</td>";
+            echo"<td><img src='img/".$un_produit['nomProduit'].".jpeg'  style='width: 100px; height: 100px; margin-right: 10px;'></td>";
+            echo"<td>".$un_produit['idProduit']."</td>";
+            echo"<td>".$un_produit['nomProduit']."</td>";
             echo"<td>".$un_produit['quantite']."</td>";
-            echo '<input type="hidden" name="idproduit" value="'.$un_produit['idproduit'].'">';
+            echo '<input type="hidden" name="idproduit" value="'.$un_produit['idProduit'].'">';
             echo"<td><input type='submit' name='delete_user' value='Supprimer'>
                         <input type='submit' name='edit_user' value='Modifier'></td>";
             echo "<tr></form>";
