c5e26e4ade41995d3fa46b79fb18621f3bea1dad
AdrienPiombini <adrien.piombini@gmail.com>
2023-01-22 13:15:32 +0100

    produit

--- Vue/espace_membre/vue_produits.php
+++ Vue/espace_membre/vue_produits.php
@@ -23,11 +23,12 @@
             echo '<input type="hidden" name="idproduit" value="'.$un_produit['idProduit'].'">';
             echo"<td>
                     <select name='qteproduit' style='width: 175px;'>
+                    <option value='0'>0</option>
                         <option value='10'>10</option>
                         <option value='50'>50</option>
                         <option value='100'>100</option>
                         <option value='500'>500</option>
-                        <option value='1000>1000</option>
+                        <option value='1000'>1000</option>
                     </select>
                         <input type='submit' name='commander_produit' value='Commander'></td>";
             echo "<tr></form>";
