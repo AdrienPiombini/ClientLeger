d569db904a7b4f60662c9bfc69a0f4223e567ae7
AdrienPiombini <adrien.piombini@gmail.com>
2023-02-07 14:12:42 +0100

    couleur a revoir

--- Vue/vue_apropos.php
+++ Vue/vue_apropos.php
@@ -21,19 +21,19 @@ Vente de véhicules d'occasion / Entretien et réparation des véhicules / Pièc
 <div class="container text-center">
   <div class="row">
     <div class="col">
-        <img src="img/Garage_meca.jpg" class="w-75 figure-img img-fluid " alt="...">
+        <img src="img/garagiste-800.png" class="w-75 figure-img img-fluid " alt="...">
     </div>
     <div class="col">
-    <figcaption class="figure-caption text-center">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error nam modi libero. Iste et veniam magni eos labore aliquid, beatae porro doloremque totam dolores illum itaque iusto rem commodi voluptates.</figcaption>
+    <figcaption class="figure-caption text-center">Le garage est un espace crucial pour votre véhicule. C'est là que vous stockez votre voiture lorsqu'elle n'est pas en usage et c'est également un endroit où vous pouvez effectuer des réparations et des entretiens. Pour assurer la sécurité de votre véhicule et pour maximiser l'espace disponible, il est important de choisir un garage fiable et bien conçu.</figcaption>
     </div>
   </div>
   <br><br>
   <div class="row">
     <div class="col">
-      <figcaption class="figure-caption text-center">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error nam modi libero. Iste et veniam magni eos labore aliquid, beatae porro doloremque totam dolores illum itaque iusto rem commodi voluptates.</figcaption>
+      <figcaption class="figure-caption text-center"> Chez FILELEC, nous offrons une variété de solutions de garage pour répondre à tous vos besoins. Que vous ayez besoin d'un espace de stationnement pour une voiture ou pour plusieurs véhicules, nous avons des options adaptées à vos besoins. Nos garages sont fabriqués avec des matériaux de haute qualité pour garantir une durabilité maximale et une protection optimale pour votre véhicule. De plus, nous offrons une installation rapide et professionnelle pour assurer un montage parfait.</figcaption>
     </div>
     <div class="col">
-        <img src="img/Garage_meca.jpg" class="w-75 figure-img img-fluid " alt="...">
+        <img src="img/choix-garagiste.jpg" class="w-75 figure-img img-fluid " alt="...">
     </div>
   </div>
 </div>
