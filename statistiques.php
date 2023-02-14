0acd39c69f1eb28b5d1978aaf85b8d2c1037182d
AdrienPiombini <adrien.piombini@gmail.com>
2023-02-14 23:46:24 +0100

    vue

--- statistiques.php
+++ statistiques.php
@@ -5,6 +5,6 @@ if (!isset($_SESSION['email'])){
 		//require_once("vue/connexion_inscription.php");
         header("Location: index.php?page=6");
 	}else if (isset($_SESSION['roles']) AND $_SESSION['roles']== 'admin'){
-    require_once("vue/espace_membre/vue_statistiques.php");
+    require_once("Vue/espace_membre/vue_statistiques.php");
     }else (require_once("erreur404.php"));
 
